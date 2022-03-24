2c1a1800f0132c90c5b2931e26c6213a0bc3e85b
Clevin M.Wilson <clevinmwilson@gmail.com>
2022-03-24 20:51:30 +0530

    add place page state added

--- src/Components/AddPlace/AddPlace.js
+++ src/Components/AddPlace/AddPlace.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TextField from "@material-ui/core/TextField";
 import './AddPlace.css'
 function AddPlace() {
+    const[place,setPlace]=useState('');
+    const handleSubmit=()=>{
+        alert(place)
+    }
     return (
         <div className='box-padding'>
             <div className="form-group">
                 <label style={{fontWeight:'600'}} className='section-title' >Add Cities</label>
-                <input type="email" className="form-control "  placeholder="🔍 Search"/>
+                <input onChange={(e)=>{
+                    setPlace(e.target.value)
+                }} type="text" className="form-control " value={place}  placeholder="🔍 Search"/>
                   
             </div>
             <div className='add-place-button-box'>
-                <button type="button" className="btn btn-primary add-place-button">Submit</button>
+                <button onClick={()=>{
+                    handleSubmit()
+                }} type="button" className="btn btn-primary add-place-button">Add</button>
             </div>
         </div>
     )
