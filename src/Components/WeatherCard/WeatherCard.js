1e3a72d68b1c4550df53355813b0f299d9752ebd
Clevin M.Wilson <clevinmwilson@gmail.com>
2022-03-26 12:50:25 +0530

    display weather in home page function added

--- src/Components/WeatherCard/WeatherCard.js
+++ src/Components/WeatherCard/WeatherCard.js
@@ -1,25 +1,41 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState ,useContext} from 'react';
 import './WeatherCard.css'
-import axios  from 'axios';
-import {useNavigate} from 'react-router-dom';
-import {WEATHER_API_KEY} from '../constants/constants'
-
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { WEATHER_API_KEY } from '../constants/constants'
+import {FirebaseContext} from '../../store/Context'
 function Card() {
-    
-    const [weather, setWeather]=useState({});
-    const navigate=useNavigate();
-    useEffect(()=>{
-        // axios.get(`http://api.weatherstack.com/current?access_key=${WEATHER_API_KEY}&query=Kannur`).then((response)=>{
-        //     setWeather(response.data)
-        // })
-    },[])
+
+    const [weather, setWeather] = useState({});
+    const navigate = useNavigate();
+    const { firebase } = useContext(FirebaseContext);
+    const [location, setLocation] = useState('');
+    useEffect(() => {
+        firebase.firestore().collection('location').get().then((snapshort) => {
+            const location = snapshort.docs.map((location) => {
+                return (
+                    {
+                        ...location.data(),
+                        id: location.id
+                    }
+                )
+
+            })
+            // axios.get(`http://api.weatherstack.com/current?access_key=${WEATHER_API_KEY}&query=${location[0].city}`).then((response) => {
+            //     setWeather(response.data)
+            // })
+        })
+    }, [])
+    useEffect(() => {
+      
+    }, [])
     return (
         <div className='box-padding'>
             {/* <h2 className='section-title'>Weather</h2> */}
             <div onClick={() => { navigate('/addplace') }} className=" mb-4 card col-md-12" >
-                <div  className="card-body ">
+                <div className="card-body ">
                     {weather.location ? <div className='row'>
-                        <div  className='col-6 mt-3'>
+                        <div className='col-6 mt-3'>
                             <h5 className=" card-title">{weather.location ? weather.location.name : null}</h5>
                             {/* <h6 className="card-subtitle mb-2 sub-title">{weather.location ? weather.location.region + " ," + weather.location.country : null}</h6> */}
                             <p className="card-text mt-3">{weather.location ? weather.current.weather_descriptions[0] : null}</p>
@@ -33,7 +49,7 @@ function Card() {
                     </div>
                         :
 
-                        <div onClick={() => { navigate('/addplace') }}  className='row'>
+                        <div onClick={() => { navigate('/addplace') }} className='row'>
                             <div className='add-place'>
                                 <h2 className='add-place-icon'>+</h2>
                             </div>
@@ -42,7 +58,7 @@ function Card() {
                 </div>
             </div>
 
-           
+
         </div>
     )
 }
