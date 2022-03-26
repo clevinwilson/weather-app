5e68933eab6f5d804e82231835e8c11b94406526
Clevin M.Wilson <clevinmwilson@gmail.com>
2022-03-26 12:29:31 +0530

    display location name function added in home page

--- src/Components/WelcomeCard/WelcomeCard.js
+++ src/Components/WelcomeCard/WelcomeCard.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React,{useContext, useEffect, useState} from 'react'
 import './WelcomeCard.css'
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import {FirebaseContext} from '../../store/Context'
 function WelcomeCard() {
+  const {firebase}=useContext(FirebaseContext);
+  const [location,setLocation]=useState('');
+  useEffect(()=>{
+    firebase.firestore().collection('location').get().then((snapshort)=>{
+      const location = snapshort.docs.map((location) => {
+        return (
+          {
+            ...location.data(),
+            id: location.id
+          }
+        )
+
+      })
+     setLocation(location[0]);
+    })
+  },[])
   return (
     <div className='box-padding'>
       <div className='welcome-box'>
@@ -14,7 +31,7 @@ function WelcomeCard() {
                 <LocationOnIcon fontSize="large"/>
               </div>
               <div className='col-9'>
-                <h5 className=" location-card-title card-title">India,Kerala,Kannur</h5>
+                <h5 className=" location-card-title card-title">{location ? location.city:null}</h5>
                 
               </div>
               
