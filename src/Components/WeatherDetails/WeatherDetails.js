904d8f0928114546ecd6cf9e0a971e2d95ed7f28
Clevin M.Wilson <clevinmwilson@gmail.com>
2022-03-29 13:16:56 +0530

    Weather details page header added

--- src/Components/WeatherDetails/WeatherDetails.js
+++ src/Components/WeatherDetails/WeatherDetails.js
@@ -1,10 +1,26 @@
 import React from 'react'
-import './WeatherDetails.css'
+import './WeatherDetails.css';
+import EditIcon from '@mui/icons-material/Edit';
+import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
+
 function WeatherDetails() {
+
   return (
-      <div className='weather-details-container'>
-        Weather details
+    <div>
+      <div className='row weather-details-container text-white header-box'>
+
+        <div className='mt-4 col-3 '>
+          <i className="material-icons nav__icon weatherpage-icon">tune</i>
+        </div>
+        <div className='mt-4 col-6 weatherdetails-header'>
+          <p>Kannur</p>
+        </div>
+        <div className='mt-4 col-3'>
+          <i className="ml-3 material-icons nav__icon weatherpage-icon">more_vert</i>
+        </div>
+      </div>
     </div>
+
   )
 }
 
