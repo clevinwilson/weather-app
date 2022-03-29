5fbda1128b40b3b24d68c990cc5ba95f52f3e954
Clevin M.Wilson <clevinmwilson@gmail.com>
2022-03-29 16:41:56 +0530

    In weather details page weather description section added

--- src/Components/WeatherDetails/WeatherDetails.js
+++ src/Components/WeatherDetails/WeatherDetails.js
@@ -6,17 +6,30 @@ import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
 function WeatherDetails() {
 
   return (
-    <div>
-      <div className='row weather-details-container text-white header-box'>
+    <div >
+      <div className='container weather-details-container text-white header-box'>
 
-        <div className='mt-4 col-3 '>
-          <i className="material-icons nav__icon weatherpage-icon">tune</i>
+        <div className='row'>
+          <div className='mt-5 col-3 '>
+            <i className="material-icons nav__icon weatherpage-icon">edit</i>
+          </div>
+          <div className='mt-4 col-6 weatherdetails-header'>
+            <p style={{ fontSize: "20px" }} >Kannur</p>
+          </div>
+          <div className='mt-4 col-3'>
+            <i className="ml-3 material-icons nav__icon weatherpage-icon">more_vert</i>
+          </div>
         </div>
-        <div className='mt-4 col-6 weatherdetails-header'>
-          <p>Kannur</p>
+
+        <div className='row'>
+          <h2 className='weather-container'>
+            <span className='weather-text'>32</span>
+            <span ><sup className='weather-format'>°c</sup></span>
+          </h2>
         </div>
-        <div className='mt-4 col-3'>
-          <i className="ml-3 material-icons nav__icon weatherpage-icon">more_vert</i>
+
+        <div className='row'>
+          <p className='weather-description'>Cloudy</p>
         </div>
       </div>
     </div>
