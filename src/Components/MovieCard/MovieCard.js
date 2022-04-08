37e4574a3df2efe973b14c0399212ce2ae18fff7
Clevin M.Wilson <clevinmwilson@gmail.com>
2022-04-08 20:48:45 +0530

    trending movies card added

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import './MovieCard.css'
+import React,{useEffect,useState} from 'react';
+import './MovieCard.css';
+import axios from '../../axios';
+import { API_KEY, imageUrl } from '../../Constants/constants'
 
 function MovieCard() {
+  const [movie, setMovie] = useState()
+  useEffect(() => {
+    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response) => {
+      console.log(response.data.results[Math.floor(Math.random() * 21)]);
+
+      setMovie(response.data.results[Math.floor(Math.random() * 19)])
+    })
+  }, [])
   return (
-      <div className='col-md-3 movie-card '>
-          <h1>Trending Movies </h1>
+    <div style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),url(${movie ? imageUrl + movie.backdrop_path : ''})` , backgroundSize:"cover"} } className='col-md-3 movie-card '>
+      <h1 className='text-white'>Trending Movies </h1>
     </div>
   )
 }
