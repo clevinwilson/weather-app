3de680b7a328e63224a00050b7a2244958579e33
Clevin M.Wilson <clevinmwilson@gmail.com>
2022-04-08 21:00:14 +0530

    loading added for movie card

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -1,7 +1,8 @@
-import React,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './MovieCard.css';
 import axios from '../../axios';
-import { API_KEY, imageUrl } from '../../Constants/constants'
+import { API_KEY, imageUrl } from '../../Constants/constants';
+import BeatLoader from "react-spinners/BeatLoader";
 
 function MovieCard() {
   const [movie, setMovie] = useState()
@@ -13,8 +14,12 @@ function MovieCard() {
     })
   }, [])
   return (
-    <div style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),url(${movie ? imageUrl + movie.backdrop_path : ''})` , backgroundSize:"cover"} } className='col-md-3 movie-card '>
-      <h1 className='text-white'>Trending Movies </h1>
+    <div style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),url(${movie ? imageUrl + movie.backdrop_path : ''})`, backgroundSize: "cover" }} className='col-md-3 movie-card '>
+      {movie ?
+        <h1 className='text-white'>Trending Movies </h1>
+        :
+        <BeatLoader color={"#EEEEEE"} loading={movie} size={17} />
+      }
     </div>
   )
 }
